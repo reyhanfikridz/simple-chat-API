e3b09b58a43c4e4bc732757ed85474675b8b1652
reyhanfikri <reyhanfikridz@gmail.com>
2022-09-11 18:31:48 +0700

    #5 Build API Create Room (POST)

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,5 +1,41 @@
 class Api::V1::RoomsController < ApplicationController
   def create
+    # check param required, if one missing, auto render Bad Request
+    params.require([:name, :flag])
+
+    # check param permitted, if there's one params not permitted, render Bad Request
+    permitted = params.permit(:name, :flag)
+    unless permitted.permitted?
+      return render json: {
+        message: "The only parameter permitted is name and flag"
+      }, status: :bad_request
+    end
+
+    # create data
+    room = nil
+    begin
+      # transaction atomic activated
+      ActiveRecord::Base.transaction do
+        # create room
+        room = Room.new({
+          name: params[:name],
+          flag: params[:flag],
+        })
+        unless room.save
+          raise StandardError.new room.errors.full_messages
+        end
+      end
+
+    rescue StandardError => e
+      return render json: {
+        message: e
+      }, status: :internal_server_error
+    end
+
+    return render json: {
+      message: "Room created successfully!",
+      data: room
+    }, status: :created
   end
 
   def index
