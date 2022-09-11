2112aea1274e789252586266214a9f648b3c6d4a
reyhanfikri <reyhanfikridz@gmail.com>
2022-09-11 11:25:14 +0700

    #3 Refactor API Create Private Messages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,13 +1,14 @@
-class Api::V1::Private::MessagesController < ApplicationController
+class Api::V1::MessagesController < ApplicationController
   def create
     # check param required, if one missing, auto render Bad Request
-    params.require([:text, :sender_id, :receiver_id])
+    params.require([:text, :sender_id, :receiver_id, :room_flag])
 
     # check param permitted, if there's one params not permitted, render Bad Request
-    permitted = params.permit(:text, :sender_id, :receiver_id, :room_id)
+    permitted = params.permit(:text, :sender_id, :receiver_id, :room_flag, :room_id)
     unless permitted.permitted?
       return render json: {
-        message: "The only parameter permitted is sender_id, receiver_id, and room_id"
+        message: "The only parameter permitted is text, sender_id, " + 
+          "receiver_id, room_flag and room_id"
       }, status: :bad_request
     end
 
@@ -16,9 +17,11 @@ class Api::V1::Private::MessagesController < ApplicationController
     begin
       # transaction atomic activated
       ActiveRecord::Base.transaction do
-        # create private room and participant first if room id not provided
+        # if flag private, create private room 
+        # and participant first if room id not provided
         room_id = params[:room_id]
-        unless room_id
+        room_flag = params[:room_flag]
+        if room_flag == "private" and not room_id
           room = Room.new({flag: "private"})
           unless room.save
             raise StandardError.new room.errors.full_messages
@@ -40,6 +43,12 @@ class Api::V1::Private::MessagesController < ApplicationController
           unless participant_receiver.save
             raise StandardError.new participant_receiver.errors.full_messages
           end
+
+        # if other then private, room_id become required
+        else
+          unless room_id
+            raise BadRequestError.new "room_id required"
+          end
         end
 
         # create private message
@@ -52,7 +61,13 @@ class Api::V1::Private::MessagesController < ApplicationController
           raise StandardError.new message.errors.full_messages
         end
       end
-    rescue => e
+
+    rescue BadRequestError => e
+      return render json: {
+        message: e
+      }, status: :bad_request
+
+    rescue StandardError => e
       return render json: {
         message: e
       }, status: :internal_server_error
@@ -91,4 +106,8 @@ class Api::V1::Private::MessagesController < ApplicationController
       message: "delete successfully!"
     }, status: :created
   end
+end
+
+# custom exception
+class BadRequestError < StandardError
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      namespace :private do
-        resources :messages
-      end
+      resources :messages
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
